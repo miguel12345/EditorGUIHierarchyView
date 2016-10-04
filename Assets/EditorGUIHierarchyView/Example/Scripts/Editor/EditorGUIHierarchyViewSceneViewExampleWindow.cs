b13427d57a9819b221f372865e2e8d346bf7980d
Miguel Ferreira <amigueloferreira@gmail.com>
2016-10-04 09:04:06 +0100

    Example : Added Scene View example

---
+++ Assets/EditorGUIHierarchyView/Example/Scripts/Editor/EditorGUIHierarchyViewSceneViewExampleWindow.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System;
+
+public class EditorGUIHierarchyViewSceneViewExampleWindow : EditorWindow
+{
+
+	EditorGUIHierarchyView hierarchyView = new EditorGUIHierarchyView();
+
+	[MenuItem ("EditorGUIHierarchyViewExampleWindow/Show Scene View")]
+	static void Init ()
+	{
+		EditorGUIHierarchyViewSceneViewExampleWindow window = (EditorGUIHierarchyViewSceneViewExampleWindow)GetWindow (typeof(EditorGUIHierarchyViewSceneViewExampleWindow));
+		window.Show ();
+	}
+
+	struct SceneGameObjectComparer : IComparer<GameObject> {
+
+		public int Compare(GameObject go1, GameObject go2) {
+			return go1.transform.GetSiblingIndex ().CompareTo (go2.transform.GetSiblingIndex ());
+		}
+	}
+
+	void OnEnable() {
+		this.titleContent = new GUIContent ("Scene View");
+	}
+
+	public void OnGUI ()
+	{
+
+		var gameObjects = SceneManager.GetActiveScene ().GetRootGameObjects ();
+		Array.Sort (gameObjects,new SceneGameObjectComparer());
+
+		hierarchyView.BeginHierarchyView ();
+		foreach (GameObject gameObject in gameObjects) {
+			gameObject.transform.GetSiblingIndex ();
+			DrawGameObject (gameObject);
+		}
+		hierarchyView.EndHierarchyView ();
+
+		Repaint ();
+	}
+
+	void DrawGameObject(GameObject go) {
+		if (go.transform.childCount > 0) {
+			hierarchyView.BeginNode (go.name);
+			foreach (Transform child in go.transform) {
+				DrawGameObject (child.gameObject);
+			}
+			hierarchyView.EndNode ();
+		} else {
+			hierarchyView.Node (go.name);
+		}
+	}
+}
+
